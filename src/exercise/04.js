3fa497280e89e3b12b013eeab46aa27600364dc1
Helene <helene.mille@absoger.fr>
2025-06-04 16:45:38 +0200

    Exercice 4 terminé

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,12 +7,15 @@ import displayText, { App, init } from "../lib/exerciseHelper";
 const exercice = () => {
 init('Fonctions fléchées')
 
-// 🐶 E = mc² : Créé une fonction fléchée `calculEnergie` avec les propietes `masse` et `celerite` (vitesse de la lumière)
+// 🐶 E = mc² : Crée une fonction fléchée `calculEnergie` avec les propiétés `masse` et `celerite` (vitesse de la lumière)
 // `celerite` aura la valeur par défaut 300000000
 //  🤖 utilise `Math.pow(celerite, 2)` pour le calcul du carré
 //  🤖 utilise `round` pour arrondir
-
+const calculEnergie = (masse, celerite = 300000000) => {
+  return Math.round(masse * Math.pow(celerite, 2));
+}
 // 🐶 appelle la fonction calculEnergie avec 5 grammes (0.005 kg)
-// et affiche le résultat à l'ecran avec displayText
+// et affiche le résultat à l'écran avec displayText
+displayText(`L'énergie d'une masse de 5 grammes est de ${calculEnergie(0.005)} Joules`);
 };
 export default () => <App exercice={exercice} />;
