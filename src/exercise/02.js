3edf93a6f3cb8acf82a97801fe4312fe3fa35c00
Helene <helene.mille@absoger.fr>
2025-06-04 16:19:28 +0200

    Exercice 2 terminé

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -7,12 +7,17 @@ import displayText, { App, init } from "../lib/exerciseHelper";
 const exercice = () => {
 init('JavaScript - Template Literals')
 
-// 🐶 Créé 3 constantes avec leurs valeurs 5 10 et 200
+// 🐶 Crée 3 constantes avec leurs valeurs 5 10 et 200
+const number1 = 5;
+const number2 = 10;
+const number3 = 200;
 
-// 🐶 utilise la fonction `Math.max` pour trouver le nombre le plus grand
+// 🐶 Utilise la fonction `Math.max` pour trouver le nombre le plus grand
 // 📑 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+const maxNumber = Math.max(number1, number2, number3);
 
-// 🐶 affiche le liblélé “Le nombre le plus grand entre 5 10 200 est 200 !”
+// 🐶 Affiche le libelé “Le nombre le plus grand entre 5 10 200 est 200 !”
+displayText(`Le nombre le plus grand entre ${number1} ${number2} ${number3} est ${maxNumber} !`);
 // à l'écran avec 'displayText'
 };
 export default () => <App exercice={exercice} />;
