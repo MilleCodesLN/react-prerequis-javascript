c7ed1f6a169d0f9a1f9070614012c1ce7ad0fae7
Helene <helene.mille@absoger.fr>
2025-06-04 16:05:07 +0200

    Exercice 1 terminé

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,22 +2,33 @@
 // ESModule Import / Export
 // http://localhost:3000/alone/exercise/01.js
 import { App } from "../lib/exerciseHelper"
+import * as roomHelper from "../exercise/helper/roomHelper";
 // 🐶 Importe 'init' et 'displayText' du module qui permet d'afficher un texte dans la page de l'exercice
 //  🤖 import displayText, { init } from "../lib/exerciseHelper";
+import displayText, { init } from "../lib/exerciseHelper";
+// 🐶 Importe le module qui permet de manipuler les pièces
 const exercice = () => {
 // 🐶 Affiche 'HelloWorld' dans la page grace à la fonction `displayText` importée
 // Atention il faut appeler une fois la fonction init avant les appels à displayText
 //  🤖 init()
 //  🤖 displayText("Hello World")
+init();
+displayText("Hello World");
 
 // 🐶 Pour la suite de l"exercice il faut aller renseigner le module dans
 // `src/helper/roomHelper.js`
 
 // 🐶 Importe tout du module roomHelper
+//  🤖 import * as roomHelper from "../helper/roomHelper";
 
 // 🐶 Utilise ces 3 fonctions pour créér les bons libéllés
 // 🤖 let lib = "Air de d'un pièce de 2 par 5 donne " + ...
 // Utilise displayText pour afficher ces 3 libéllés
-
-};
+let lib = "Air d'une pièce de 2 par 5 donne " + roomHelper.calculAir(2, 5);
+displayText(lib);
+lib = "Périmètre d'une pièce de 2 par 5 donne " + roomHelper.calculPerimetre(2, 5);
+displayText(lib);
+lib = "Température maximale est " + roomHelper.TEMP_MAX;
+displayText(lib);
+};  
 export default () => <App exercice={exercice} />;
--- src/exercise/helper/roomHelper.js
+++ src/exercise/helper/roomHelper.js
@@ -1,8 +1,16 @@
 // 🐶 créé une fonction `calculAir` (air = longeur x largeur) qui sera exportée par defaut
 // 🤖 export default function
+const calculAir = (longueur, largeur) =>{
+  return longueur * largeur;
+}
 
 // 🐶 créé une fonction `calculPerimetre` (perimètre = longeur x largeur x 2) qui sera exportée
 // 🤖 export function
-
+function calculPerimetre(longueur, largeur) {
+  return (longueur + largeur) * 2;
+}   
 // 🐶 créé une constante `TEMP_MAX` = 60 qui sera exportée
 // 🤖 export const
+const TEMP_MAX = 60;
+
+export { calculAir, calculPerimetre, TEMP_MAX };
